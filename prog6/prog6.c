18e6a3b19e1fc3bb5b17d4a704c51a6cf0fb3af6
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-05-23 19:50:36 +0530

    prg6.c done

--- prog6/prog6.c
+++ prog6/prog6.c
@@ -1,27 +1,70 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef struct circle{
+typedef struct{
     int center_x;
     int center_y;
     int radius;
-};
+}circle;
 
-void initialize_buffer(int heigth, int width, char buffer[heigth][width]);
-void draw_circle(int heigth, int widht, char buffer[heigth][width], circle c);
-void print_buffer(int heigth, int width, char buffer[heigth][width]);
+void initialize_buffer(int height, int width, char buffer[height][width]);
+void draw_circle(int height, int width, char buffer[height][width], circle c);
+void print_buffer(int height, int width, char buffer[height][width]);
 float calculate_distance(int x1, int x2, int y1, int y2);
 
 int main() {
-    int width, heigth;
-    printf("Enter circle width:");
+    int width, height;
+    printf("Enter circle width: ");
     scanf("%d", &width);
 
-    printf("Enter circle heigth:");
+    printf("Enter circle height: ");
     scanf("%d", &height);
 
     char buffer[height][width];
 
     circle c;
+
+    c.center_x = width/2;
+    c.center_y = height/2;
+
+    printf("Enter radius: ");
+    scanf("%d", &c.radius);
+
+    initialize_buffer(height, width, buffer);
+    draw_circle(height, width, buffer, c);
+    print_buffer(height, width, buffer);
+
     return 0;
+}
+
+void initialize_buffer(int height, int width, char buffer[height][width]){
+    for(int i=0; i<height; i++){
+        for(int j=0; j<width; j++){
+            buffer[i][j] = ' ';
+        }
+    }
+}
+
+float calculate_distance(int x1, int x2, int y1, int y2){
+    return sqrt(pow(y2 - y1, 2)+ pow(x2 - x1, 2));
+}
+
+void draw_circle(int height, int width, char buffer[height][width], circle c){
+    for(int y=0; y<height; y++){
+        for(int x=0; x<width; x++){
+            float distance = calculate_distance(x, c.center_x, y, c.center_y);
+
+            if (distance >= c.radius - 0.5 && distance <= c.radius + 0.5)
+                buffer[y][x] = '*';
+        }
+    }
+}
+
+void print_buffer(int height, int width, char buffer[height][width]){
+    for(int i=0; i<height; i++){
+        for(int j=0; j<width; j++){
+            printf("%c", buffer[i][j]);
+        }
+        printf("\n");
+    }
 }
